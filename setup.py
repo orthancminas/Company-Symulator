0c3a850c19dc97600a6b33f9c8139353a21a5e82
Alan Cesarski (Orthanc Minas) <orthanc.minas@gmail.com>
2015-08-01 18:09:38 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='com.orthanc.comsys',
+    version='1.0 alpha',
+    packages=[''],
+    url='',
+    license='Apache License 2.0',
+    author='Orthanc Minas',
+    author_email='orthanc.minas@gmail.com',
+    description=''
+)
